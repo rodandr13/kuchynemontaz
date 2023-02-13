84609ddd5028ca1ee8becd3af7e095427138ab39
andrey <rodandr13@gmail.com>
2023-02-13 18:26:48 +0300

    feat: add popup script

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+let popup = document.querySelector('.popup');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
