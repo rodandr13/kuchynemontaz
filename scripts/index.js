9130cf81b5014cce90f3b8d011d1f5aace30698a
andrey <rodandr13@gmail.com>
2023-02-17 17:02:23 +0300

    feat: add open close script for popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 let popup = document.querySelector('.popup');
+let popupCloseBtn = document.querySelector('.popup__close-button');
+let contactFormBtn = document.querySelector('.button_type_contact');
+
 
 function popupOpen() {
   popup.classList.add('popup_opened');
@@ -7,4 +10,7 @@ function popupOpen() {
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
+
+contactFormBtn.addEventListener('click', popupOpen);
+popupCloseBtn.addEventListener('click', popupClose);
 
