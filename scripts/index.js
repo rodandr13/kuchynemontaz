e6f3c5ee23e33f5c1cf915e1a652a9fb2ad9c5a7
andrey <rodandr13@gmail.com>
2023-03-15 11:51:12 +0300

    fear: edit switcher

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ let popup = document.querySelector('.popup');
 let popupCloseBtn = document.querySelector('.popup__close-button');
 let contactFormBtn = document.querySelectorAll('.button_type_contact');
 
+const langSwitcher = document.querySelector('.lang-switcher');
+
 function popupOpen() {
   popup.classList.add('popup_opened');
 }
