5af8ddae3dc11537b21c8a643f0be5a25f245b87
andrey <rodandr13@gmail.com>
2023-03-19 20:37:20 +0300

    feat: add popup script

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,19 @@ const popupFormButton = document.querySelectorAll('.button_style_contact');
 const questionElements = document.querySelectorAll('.question');
 const popupImage = document.querySelector('.popup_type_image');
 const certificateElements = document.querySelectorAll('.certificates__link');
+const workElements = document.querySelectorAll('.work');
 
 const openImagePopup = (src) => {
   popupImage.querySelector('.popup__image').src = src;
   popupImage.classList.add('popup_opened');
 }
+
+workElements.forEach((work) => {
+  work.addEventListener('click', (evt) => {
+    const srcImage = evt.currentTarget.querySelector('.work__image').src;
+    openImagePopup(srcImage);
+  })
+})
 
 certificateElements.forEach((certificate) => {
   certificate.addEventListener('click', (evt) => {
