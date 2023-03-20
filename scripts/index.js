7211b28f71666bb461cfffbb34b0eeaf643267f2
andrey <rodandr13@gmail.com>
2023-03-20 20:31:47 +0300

    feat: add close popup script

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 let popupSendRequest = document.querySelector('.popup_type_send-request');
 let closeButtons = document.querySelectorAll('.popup__close-button');
+
+const popupList = document.querySelectorAll('.popup');
 const popupFormButton = document.querySelectorAll('.button_style_contact');
 const questionElements = document.querySelectorAll('.question');
 const popupImage = document.querySelector('.popup_type_image');
 const certificateElements = document.querySelectorAll('.certificates__link');
 const workElements = document.querySelectorAll('.work');
+
 
 const openImagePopup = (src) => {
   popupImage.querySelector('.popup__image').src = src;
@@ -33,19 +36,43 @@ questionElements.forEach((question) => {
   });
 });
 
-function popupOpen() {
-  popupSendRequest.classList.add('popup_opened');
+const handlerKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_opened');
+    closePopup(popup);
+  }
 }
 
-const popupClose = (popup) => popup.classList.remove('popup_opened');
+function openPopup(popup) {
+  document.addEventListener('keydown', handlerKeyDown);
+  popup.classList.add('popup_opened');
+}
+
+const closePopup = (popup) => {
+  document.removeEventListener('keydown', handlerKeyDown);
+  popup.classList.remove('popup_opened');
+}
+
+const openPopupAddCard = () => {
+  popupSendRequest.querySelector('form').reset();
+  openPopup(popupSendRequest);
+}
+
+const closePopupOverlayClick = (evt) => {
+  if (evt.target.classList.contains('popup')) {
+    closePopup(evt.target);
+  }
+}
 
 popupFormButton.forEach(function (button) {
-  button.addEventListener('click', popupOpen);
+  button.addEventListener('click', openPopupAddCard);
 })
 closeButtons.forEach((button) => {
   const popup = button.closest('.popup')
   button.addEventListener('click', () => {
-    popupClose(popup);
+    closePopup(popup);
   })
 });
-
+popupList.forEach((popup) => {
+  popup.addEventListener('click', closePopupOverlayClick);
+});
