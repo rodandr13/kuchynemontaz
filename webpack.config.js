d53eb93746c0fcbc91027d29636864b1c9b21203
andrey <rodandr13@gmail.com>
2023-04-13 20:21:25 +0300

    feat: optimized images

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: './scripts/main.js',
     publicPath: '',
   },
   mode: 'development',
@@ -29,14 +29,14 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name].[ext]',
+          filename: './images/[name].[ext]',
         }
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name].[ext]',
+          filename: './fonts/[name].[ext]',
         }
       },
       {
